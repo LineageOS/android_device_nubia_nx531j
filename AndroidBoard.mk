14bf1b6731605401d21332c830994751b9c6bc05
XiNGRZ <chenxingyu92@gmail.com>
2016-08-03 14:53:09 +0800

    nx531j: build hal with kernel headers only
    Change-Id: I0ca6b2d9df26618da4943410384abfecac6dcd1e

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+include device/zte/nx531j/kernel/AndroidKernel.mk
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP) $(TARGET_PREBUILT_KERNEL_INCLUDE)
+	$(transform-prebuilt-to-target)
 
 # include the non-open-source counterpart to this file
 -include vendor/zte/nx531j/AndroidBoardVendor.mk
