af400b1e5ac2d5f2d412550201fa4f6ea13655eb
XiNGRZ <chenxingyu92@gmail.com>
2016-08-05 17:10:23 +0800

    nx531j: use NQNFC

--- device_nx531j.mk
+++ device_nx531j.mk
@@ -230,8 +230,13 @@ PRODUCT_COPY_FILES += \
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    NfcNci \
-    nfc_nci.pn54x.default \
+    com.nxp.nfc.nq \
+    libnqnfc-nci \
+    libp61-jcop-kit \
+    nfc_nci.nqx.default \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
+    NQNfcNci \
     Tag
 
 PRODUCT_COPY_FILES += \
